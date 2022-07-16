2e1d736b9ed61fc2146b687b5ee09164f95988e9
Isaiah <ijandicos90@gmail.com>
2022-07-15 23:36:50 -0400

    each method not working

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require "pry"
+
+katz_deli = []
+
+
+def take_a_number(katz_que, customer)
+
+katz_que.push(customer)
+puts "Welcome, #{customer}. you are number #{katz_que.index(customer) + 1} in line."
+end
+
+def line(katz_que)
+current_que = katz_que.each_with_index { |customer, index| " #{index}. #{customer}"}
+if katz_que.length == 0 
+    puts "The line is currently empty" 
+else 
+    puts "The line is currently:" + " #{current_que.join(" ")}"
+end
+end
+
+def now_serving(katz_que)
+    if deli.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_que.shift()}."
+       
+    end
+end
+
